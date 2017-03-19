bfe2eeb2e730efbdad6c5e19ec0d6fd6e5dab810
menjaw <menjaholm@gmail.com>
2017-03-19 15:48:14 +0100

    init commit of the project

---
+++ src/main/webapp/Javascript/basicJavascript.js
@@ -0,0 +1,40 @@
+/* 
+ * The code in this file is placed by the hierarchy it is being printed out.
+ * 
+ * When write-method is used it will be printed out after the methods that use innerHTML
+ */
+
+//This is a single-line comment in javascript
+/*This is a multiplied comment in javascript*/
+
+
+
+//Statements that refers to div-id 'statement' in the HTML page
+document.getElementById("statements").innerHTML = "<h3>I love Hummus</h3> <h3>and Hummus love me</h3>";
+
+
+
+//Variable types
+var product = "Milk";
+var price = 20;
+var discount = 9.54;
+var newPrice = price - discount;
+
+//En anden måde at skrive DISCOUNT OG NEW PRICE på (præcis det samme)
+//var discount = -9.54;
+//var newPrice = price + (discount);
+
+document.getElementById("variableTypes").innerHTML = "You have to pay: " + "<b>" + price + " kr. " + "</b>" + "for " + "<b>" + product + "</b>";
+document.write("You got a discount on: " + "<b>" + discount + " kr." + "</b>" + "<br>");
+document.write("Your new price is: " + "<b>" + newPrice + " kr." + "</b>" + " for the " + "<b>" + product + "</b>" + "<hr>");
+
+
+
+//Statements that is printed out on the HTML page
+document.write("<h1>Hey brown cow!</h1> <h2>What is the date now?</h2>" + Date() + "<hr>");
+
+
+
+//Variable that use write-method to print out result
+var myVariable = 30;
+document.write("<h4> This is my variable: " + myVariable + "</h4>" + "<hr>");
---
+++ target/NewBostonJavascript-1.0-SNAPSHOT/Javascript/functions.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function confirm() {
+    alert("Are you sure you want to see the form");
+}
+
+function showForm(){
+    document.getElementById("showForm").innerHTML = "<form>" + 
+                                                    "<p>Name:</p>" +
+                                                    "<input type=\"text\">" +
+                                                    "<p> Address:</p>" +
+                                                    "<input type=\"text\">" +
+                                                    "<p>City:</p>" +
+                                                    "<input type=\"text\">" +
+                                                    "<p>Country:</p>" +
+                                                    "<input type=\"text\">" +
+                                                    "<br></br>" +
+                                                    "<input type=\"button\" value=\"Submit\">" +
+                                                    "</form>";
+}
